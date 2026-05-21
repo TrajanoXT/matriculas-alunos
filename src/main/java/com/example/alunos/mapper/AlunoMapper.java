5891fb835ae95847fdba062dfc68d83e9a4ccc78
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-05-21 06:21:51 -0300

    Add domain and service source files

---
+++ src/main/java/com/example/alunos/mapper/AlunoMapper.java
@@ -0,0 +1,38 @@
+package com.example.alunos.mapper;
+
+import com.example.alunos.dto.AlunoRequest;
+import com.example.alunos.dto.AlunoResponse;
+import com.example.alunos.dto.MatriculaDTO;
+import com.example.alunos.entities.Aluno;
+import com.example.alunos.entities.Matricula;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AlunoMapper {
+    public Aluno toEntity(AlunoRequest request) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(request.nome());
+        aluno.setDataNascimento(request.dataNascimento());
+        aluno.setTelefone(request.telefone());
+
+        List<Matricula> matriculas = request.matriculas().stream().map(m->{
+            Matricula matricula = new Matricula();
+            matricula.setCodigoMatricula(m.codigoMatricula());
+            matricula.setDataInicio(m.dataInicio());
+            matricula.setNomeCurso(m.nomeCurso());
+            matricula.setAluno(aluno);
+            return matricula;
+        }).toList();
+        aluno.setMatriculas(matriculas);
+        return aluno;
+    }
+
+    public AlunoResponse toResponse(Aluno aluno) {
+        List<MatriculaDTO> matriculaDTOS = aluno.getMatriculas().stream().map(m->
+                new MatriculaDTO(m.getCodigoMatricula(),m.getNomeCurso(), m.getDataInicio())).toList();
+        return new AlunoResponse(aluno.getId(), aluno.getNome(), aluno.getTelefone(), aluno.getDataNascimento(), matriculaDTOS);
+    }
+}
