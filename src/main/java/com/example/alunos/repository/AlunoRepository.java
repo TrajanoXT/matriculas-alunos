c35fb6faf546e48972f063a0aad1246a6b25fe59
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-05-21 06:21:52 -0300

    Add domain and service source files

---
+++ src/main/java/com/example/alunos/repository/AlunoRepository.java
@@ -0,0 +1,6 @@
+package com.example.alunos.repository;
+
+import com.example.alunos.entities.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {}
