4df2b46db7175ebd579b5c0854218496b5ab297e
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-05-21 06:21:17 -0300

    Add application source files

---
+++ src/main/java/com/example/alunos/dto/MatriculaDTO.java
@@ -0,0 +1,10 @@
+package com.example.alunos.dto;
+
+import java.time.LocalDate;
+
+public record MatriculaDTO(
+        String codigoMatricula,
+        String nomeCurso,
+        LocalDate dataInicio
+) {
+}
