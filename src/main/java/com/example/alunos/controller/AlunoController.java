e75f123563a34077d28205e45b049800cb83542f
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-05-21 06:21:13 -0300

    Add application source files

---
+++ src/main/java/com/example/alunos/controller/AlunoController.java
@@ -0,0 +1,44 @@
+package com.example.alunos.controller;
+
+import com.example.alunos.dto.AlunoRequest;
+import com.example.alunos.dto.AlunoResponse;
+import com.example.alunos.dto.MatriculaDTO;
+import com.example.alunos.service.AlunoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/alunos")
+public class AlunoController {
+    private final AlunoService alunoService;
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AlunoResponse> criar(@RequestBody AlunoRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(alunoService.salvar(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AlunoResponse>> listar(){
+        return ResponseEntity.ok(alunoService.listarTodos());
+    }
+    @GetMapping("/{id}/matriculas")
+    public List<MatriculaDTO> listarMatriculas(@PathVariable Long id){
+        return alunoService.listarMatriculas(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AlunoResponse> atualizar(@PathVariable Long id, @RequestBody AlunoRequest request){
+        return ResponseEntity.ok(alunoService.atualizar(id, request));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> remover(@PathVariable Long id){
+        alunoService.remover(id);
+        return ResponseEntity.noContent().build();
+    }
+}
