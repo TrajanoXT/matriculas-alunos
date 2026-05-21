8874d781169235414ac62d381131becbd75f7161
Vinicius Trajano <viniciustrajano46@gmail.com>
2026-05-21 06:21:18 -0300

    Add application source files

---
+++ src/main/java/com/example/alunos/entities/Aluno.java
@@ -0,0 +1,28 @@
+package com.example.alunos.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Aluno extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String telefone;
+    private String email;
+    private LocalDate dataNascimento;
+
+    @OneToMany(mappedBy = "aluno",cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Matricula> matriculas=new ArrayList<>() {
+    };
+}
